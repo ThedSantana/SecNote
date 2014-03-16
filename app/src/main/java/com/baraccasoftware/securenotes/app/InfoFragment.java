a2c912a18594831feec0e3e909ee1e92dcd02a54
chemickypes <mor.angelo.mor@gmail.com>
2014-03-16 02:09:44 +0100

    "primo commit"

---
+++ app/src/main/java/com/baraccasoftware/securenotes/app/InfoFragment.java
@@ -0,0 +1,10 @@
+package com.baraccasoftware.securenotes.app;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by angelo on 25/02/14.
+ */
+public class InfoFragment extends Fragment {
+
+}
